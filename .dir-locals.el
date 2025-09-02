a16c251849ac541f65e0232d40dc84b500d42b4b
Abhimanyu Govindaraju <49420663+abhimanyu-g@users.noreply.github.com>
2025-09-02 22:25:24 +0530

    Updating dir locals
    - Fetch project root path from project.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((org-hugo-base-dir . "../")))
- ("content-org/"
-  . ((org-mode . ((eval . (org-hugo-auto-export-mode)))))))
+((org-mode . ((eval . (setq org-hugo-base-dir (project-root (project-current))))))
+ ("content-org/" . ((org-mode . ((eval org-hugo-auto-export-mode))))))
